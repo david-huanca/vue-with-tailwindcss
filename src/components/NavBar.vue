40407ef742b0256111e449f586a20d18b745832b
James Perkins <jamesperkins@hey.com>
2021-02-11 16:03:50 -0500

    Creating a responsive navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="border-b">
+    <div
+      class="container flex flex-col flex-wrap p-5  md:items-center md:flex-row "
+    >
+      <nav>
+        <ul class="flex flex-wrap items-center justify-center text-base ">
+          <li class="mr-5 text-sm font-semibold hover:text-gray-800">
+            <router-link to="/">Home</router-link>
+          </li>
+          <li class="mr-5 text-sm font-semibold hover:text-gray-800">
+            <router-link to="/">Blog</router-link>
+          </li>
+          <li class="mr-5 text-sm font-semibold hover:text-gray-800">
+            <router-link to="about">About</router-link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
