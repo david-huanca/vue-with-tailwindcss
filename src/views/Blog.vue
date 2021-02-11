1b6a1dd51b092e8d58d9e4cf6c271b1b3c16d9e4
James Perkins <jamesperkins@hey.com>
2021-02-11 17:56:22 -0500

    Adding Blog Page

---
+++ src/views/Blog.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="blog">
+    <BlogPage />
+  </div>
+</template>
+
+<script>
+import BlogPage from "@/components/BlogPage.vue";
+
+export default {
+  name: "Blog",
+  components: {
+    BlogPage,
+  },
+};
+</script>
